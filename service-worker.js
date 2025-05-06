f7b566c63005e62ed83023ff5426b4511ffd3b78
Marty <rosary-vassals8m@icloud.com>
2025-05-06 04:39:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,7 +26,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
